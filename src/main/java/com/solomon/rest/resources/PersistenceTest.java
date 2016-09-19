b21601d6f0fb3d9efa6ad2504fc4e1cf70fb9c2e
Gaurav Diwanji <gauravd@tamu.edu>
2016-09-18 22:09:30 -0500

    JPA - unsuccessful

---
+++ src/main/java/com/solomon/rest/resources/PersistenceTest.java
@@ -0,0 +1,35 @@
+package com.solomon.rest.resources;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+import com.solomon.rest.model.*;
+
+public class PersistenceTest {
+	
+	private static final String PERSISTENCE_UNIT_NAME = "JPATEST";
+    private static EntityManagerFactory factory;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+        
+        em.getTransaction().begin();
+        Test_table test = new Test_table();
+        test.setValue_field("Test");
+        em.persist(test);
+        em.getTransaction().commit();
+
+        em.close();
+        
+        System.out.println("Successful");
+
+	}
+
+}
